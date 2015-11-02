b7cec65946d1f8ede4f9223b24baf7b353a926b8
Floyd14 <andreafloyd14@icloud.com>
2015-11-02 14:05:01 +0100

    Update: add train.csv, implementation of Data project - Titanic
    Signed-off-by: Floyd14 <andreafloyd14@icloud.com>

---
+++ Python - Data Analisi Udemy Course/Data project - Titanic.py
@@ -0,0 +1,32 @@
+###
+#
+#
+#If you want, go ahead and just go for it with these free public data set resources:
+#
+# http://www.data.gov/
+#
+# http://aws.amazon.com/public-data-sets/
+#
+# http://www.google.com/publicdata/directory
+#
+# Import train.csv from:
+# https://www.kaggle.com/c/titanic-gettingStarted
+#
+###
+
+#Now let's open it with pandas
+import pandas as pd
+from pandas import Series,DataFrame
+
+# Set up the Titanic csv file as a DataFrame
+titanic_df = pd.read_csv('train.csv')
+
+# Let's see a preview of the data
+print(titanic_df.head())
+
+# We could also get overall info for the dataset
+print(titanic_df.info())
+
+# Now let's get a simple visualization!
+#sns.factorplot('Alone',data=titanic_df,palette='Blues')
+
